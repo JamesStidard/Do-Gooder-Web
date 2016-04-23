bfcc512690c163d2301ef63a51012a24bc1ee6c1
jamesstidard <jamesstidard@gmail.com>
2016-04-23 21:10:48 +0100

    percentage count

--- app/components/percentage-widget/percentage-widget.js
+++ app/components/percentage-widget/percentage-widget.js
@@ -1,5 +1,6 @@
 import tmpl from './percentage-widget.html!text'
 import Vue from 'vue'
+import range from 'app/utils/range'
 
 
 export default Vue.extend({
@@ -14,15 +15,19 @@ export default Vue.extend({
             default: 100,
         },
         primary_color: {
+            type: String,
             default: 'white',
         },
         secondary_color: {
+            type: String,
             default: 'black',
         },
         background_color: {
+            type: String,
             default: 'red',
         },
         transition_speed: {
+            type: Number,
             default: 1,
         },
         insert_size: {
@@ -40,8 +45,23 @@ export default Vue.extend({
             type: Boolean,
             default: false,
         },
+        show_text: {
+            type: Boolean,
+            default: true,
+        },
+        font_size: {
+            type: Number,
+            default: 22,
+        },
+        text_width: {
+            type: Number,
+            default: 56,
+        },
     },
     computed: {
+        numbers() {
+            return ['-', ...range(0, 101)]
+        },
         radial_progress_style() {
             const scale = this.clockwise ? '1' : '-1'
             return {
@@ -65,7 +85,7 @@ export default Vue.extend({
         },
         mask_style() {
             return {
-                'clip': 'rect(0px, ' + this.circle_size + 'px, ' + this.circle_size + 'px, ' + this.circle_size / 2 + 'px)',
+                'clip': 'rect(0px, ' + this.circle_size + 'px, ' + this.circle_size + 'px, ' + (this.circle_size / 2) + 'px)',
             }
         },
         rotation() {
@@ -83,7 +103,7 @@ export default Vue.extend({
         fill_style() {
             return {
                 'background-color': this.primary_color,
-                'clip': 'rect(0px, ' + this.circle_size/2 + 'px, ' + this.circle_size + 'px, 0px)',
+                'clip': 'rect(0px, ' + (this.circle_size / 2) + 'px, ' + this.circle_size + 'px, 0px)',
             }
         },
         inset_style() {
@@ -91,8 +111,8 @@ export default Vue.extend({
                 'width': this.insert_size + 'px',
                 'height': this.insert_size + 'px',
                 'position': 'absolute',
-                'margin-left': (this.circle_size - this.insert_size) / 2 + 'px',
-                'margin-top': (this.circle_size - this.insert_size) / 2 + 'px',
+                'margin-left': ((this.circle_size - this.insert_size) / 2) + 'px',
+                'margin-top': ((this.circle_size - this.insert_size) / 2) + 'px',
                 'background-color': this.background_color,
                 'border-radius': '50%',
             }
@@ -103,7 +123,37 @@ export default Vue.extend({
                 'height': this.circle_size + 'px',
                 'position': 'absolute',
                 'background-color': this.background_color,
-                'clip': 'rect(0px, ' + this.circle_size/2 + 'px, ' + this.circle_size + 'px, 0px)',
+                'clip': 'rect(0px, ' + (this.circle_size / 2) + 'px, ' + this.circle_size + 'px, 0px)',
+            }
+        },
+        percentage_text_style() {
+            return {
+                'width': this.text_width + 'px',
+                'height': this.font_size + 'px',
+                'position': 'absolute',
+                'top': ((this.insert_size - this.font_size) / 2) + 'px',
+                'left': ((this.insert_size - this.text_width) / 2) + 'px',
+                'line-height': 1,
+                'text-align': 'left',
+                'color': this.primary_color,
+                'font-size': this.font_size + 'px',
+                'transition': 'width ' + this.transition_speed + 's',
+                'overflow': 'hidden',
+            }
+        },
+        numbers_style() {
+            return {
+                'margin-top': (-1 * this.font_size) + 'px',
+                'transition': 'width ' + this.transition_speed + 's',
+                'width': (this.percentage * this.text_width + this.text_width) + 'px',
+            }
+        },
+        span_style() {
+            return {
+                'width': this.text_width + 'px',
+                'display': 'inline-block',
+                'vertical-align': 'top',
+                'text-align': 'center',
             }
         },
     },
